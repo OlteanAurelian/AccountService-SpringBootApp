ff69ad09f53198410f9231b54370cd7376379671
Aurelian <aurelian.oltean@yahoo.com>
2020-03-25 16:12:36 +0200

    Add logging

--- src/main/java/com/aurelian/application/services/ExchangeRateService.java
+++ src/main/java/com/aurelian/application/services/ExchangeRateService.java
@@ -3,6 +3,7 @@ package com.aurelian.application.services;
 import com.aurelian.application.entities.ExchangeRate;
 import com.aurelian.application.entities.ExchangeRatesResponse;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
 
+@Log4j2
 @Service
 public class ExchangeRateService {
     @Autowired
@@ -18,17 +20,16 @@ public class ExchangeRateService {
     @HystrixCommand(fallbackMethod = "fallback")
     @Cacheable("exchangeRate")
     public ExchangeRate getExchangeRateByCurrency(String currency) {
-        System.out.println("Going to the rates-ws for exchange rates for currency: " + currency);
+        log.info("Going to the rates-ws for exchange rates for currency {}", currency);
         String url = "https://api.exchangeratesapi.io/latest";
-
         ExchangeRatesResponse response = restTemplate.getForObject(url, ExchangeRatesResponse.class);
-        System.out.println(response);
+        log.info("ExchangeRatesResponse {}", response);
 
         return new ExchangeRate(currency, Objects.requireNonNull(response).getRates().get(currency));
     }
 
     public ExchangeRate fallback(String currency, Throwable hystrixCommand){
-        System.out.println("Enter fallback for currency: " + currency);
+        log.info("Enter fallback for currency {}", currency);
         return new ExchangeRate(currency, 1);
     }
 }
